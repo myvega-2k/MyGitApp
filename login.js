7316db2fa9cfa6c770c77abaa787ccbb419e07a7
myvega-2k <myvega2k@gmail.com>
2024-09-04 15:41:14 +0900

    login.js auth username,password 체크

--- login.js
+++ login.js
@@ -2,5 +2,10 @@ export default function auth(user, pass) {
     console.log('인증처리하기');
     if(user){
         console.log('username 있음');
+    }else {
+        console.log('username 없음');
+    }
+    if(pass){
+        console.log('password 있음');
     }
 }
